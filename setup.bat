8c65280b175b378b17fd62c140b578548a032357
Pavel Bastov <pbastov@gmail.com>
2015-05-28 11:16:52 +0600

    Add setup.bat

---
+++ setup.bat
@@ -0,0 +1,10 @@
+git config --global user.name "Pavel Bastov"
+git config --global user.email "pbastov@gmail.com"
+git config --global credential.helper wincred
+git config --global alias.st status
+git config --global alias.lga "log --oneline --graph --decorate"
+git config --global alias.df diff
+git config --global alias.dc "diff --cache"
+git config --global alias.ci commit
+git config --global alias.co checkout
+
